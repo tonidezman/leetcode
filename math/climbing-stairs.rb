c728dd097f447667483489aa66fb3ede1a3208a3
Toni Dezman <toni.dezman@gmail.com>
2025-02-01 11:46:55 +0100

    LC-climbing-stairs [Runtime: 0 ms; Memory: 211.5 MB]

--- math/climbing-stairs.rb
+++ math/climbing-stairs.rb
@@ -1,7 +1,9 @@
 # @param {Integer} n
 # @return {Integer}
-def climb_stairs(n, step=0)
+def climb_stairs(n, step=0, memo={})
+    return memo[step] if memo.include?(step)
     return 0 if step > n
     return 1 if step == n
-    return climb_stairs(n, step + 1) + climb_stairs(n, step + 2)
+    memo[step] = climb_stairs(n, step + 1, memo) + climb_stairs(n, step + 2, memo)
+    return memo[step]
 end
