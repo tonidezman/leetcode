546ff9de1d7f00bf0dd6c7af23cfef91b1f98344
Toni Dezman <toni.dezman@gmail.com>
2025-02-02 09:54:20 +0100

    LC-palindrome-linked-list [Runtime: 32 ms; Memory: 227.4 MB]

---
+++ linked-list/palindrome-linked-list.rb
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {Boolean}
+def is_palindrome(head)
+    curr = head
+    res = []
+    while curr
+        res << curr.val
+        curr = curr.next
+    end
+    res == res.reverse
+end
