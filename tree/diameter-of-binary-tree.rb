78144a85b4d1e9b224c62e4bbcc66f90680abdf5
Toni Dezman <toni.dezman@gmail.com>
2025-02-02 08:28:00 +0100

    LC-diameter-of-binary-tree [Runtime: N/A; Memory: N/A]

--- tree/diameter-of-binary-tree.rb
+++ tree/diameter-of-binary-tree.rb
@@ -8,23 +8,17 @@
 #     end
 # end
 
-def get_length(node, curr=0)
-    return curr if node.nil?
-    is_leaf = node.left.nil? && node.right.nil?
-    return 1 if is_leaf
-    left = get_length(node.left, curr)
-    right = get_length(node.right, curr)
-    return 1 + [left, right].max
+def get_depth(node)
+    return 0 if node.nil?
+    return 1 + [get_depth(node.left), get_depth(node.right)].max
 end
 
 # @param {TreeNode} root
 # @return {Integer}
-def diameter_of_binary_tree(root, res=0)
+def diameter_of_binary_tree(root)
     return 0 if root.nil?
-    left = get_length(root.left)
-    right = get_length(root.right)
-    res = [res, left+right].max
-    diameter_of_binary_tree(root.left, res)
-    diameter_of_binary_tree(root.right, res)
-    res
+    left = get_depth(root.left)
+    right = get_depth(root.right)
+    res = left + right
+    [res, diameter_of_binary_tree(root.left), diameter_of_binary_tree(root.right)].max
 end
