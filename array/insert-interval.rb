e75793471b683fc2822946e1245fb5b4fd0a2086
Toni Dezman <toni.dezman@gmail.com>
2025-02-03 09:42:34 +0100

    LC-insert-interval [Runtime: 4 ms; Memory: 212.3 MB]

---
+++ array/insert-interval.rb
@@ -0,0 +1,20 @@
+# @param {Integer[][]} intervals
+# @param {Integer[]} new_interval
+# @return {Integer[][]}
+def insert(intervals, new_interval)
+    res = []
+    intervals.each.with_index do |interval, i|
+        if new_interval[1] < interval[0]
+            res << new_interval
+            return res + intervals[i..-1]
+        end
+
+        if interval[1] < new_interval[0]
+            res << interval
+        else
+            new_interval = [[interval[0], new_interval[0]].min, [interval[1], new_interval[1]].max]
+        end
+    end
+    res << new_interval
+    res
+end
