82f6af8f33b90220567cc57b8deb1d7a2d06b0ef
Toni Dezman <toni.dezman@gmail.com>
2025-02-22 18:48:15 +0100

    LC-rotate-array [Runtime: 7 ms; Memory: 25.9 MB]

--- array/rotate-array.py
+++ array/rotate-array.py
@@ -1,12 +1,14 @@
+def my_reverse(arr, i, j):
+    while i < j:
+        # swap
+        arr[i], arr[j] = arr[j], arr[i]
+        i += 1
+        j -= 1
+
+
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         k %= len(nums)
-        my_reverse(nums, 0, len(nums)-1)
-        my_reverse(nums, 0, k-1)
-        my_reverse(nums, k, len(nums)-1)
-
-def my_reverse(nums, i, j):
-    while i < j:
-        nums[i], nums[j] = nums[j], nums[i]
-        i += 1
-        j -= 1
+        my_reverse(nums, i=0, j=len(nums)-1)
+        my_reverse(nums, i=0, j=k-1)
+        my_reverse(nums, i=k, j=len(nums)-1)
