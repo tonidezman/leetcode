6dd8260e4e0f81560699035ea78ccfc7bc466bec
Toni Dezman <toni.dezman@gmail.com>
2025-03-01 09:43:47 +0100

    LC-employees-earning-more-than-their-managers [Runtime: 327 ms; Memory: 0B]

--- database/employees-earning-more-than-their-managers.sql
+++ database/employees-earning-more-than-their-managers.sql
@@ -1,2 +1,6 @@
 -- Write your PostgreSQL query statement below
-select * from employee;
+select e.name as Employee
+from employee e
+left join employee m
+    on e.managerId = m.id
+where e.salary > m.salary
