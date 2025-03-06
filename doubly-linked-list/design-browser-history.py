e6a7cf96d8f7cbd2dd4b3afc2718f871e5aae7f2
Toni Dezman <toni.dezman@gmail.com>
2025-03-06 10:26:06 +0100

    LC-design-browser-history [Runtime: N/A; Memory: N/A]

---
+++ doubly-linked-list/design-browser-history.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.homepage = homepage
+        self.curr = Node(homepage)
+
+    def visit(self, url: str) -> None:
+        new_node = Node(url)
+        self.curr.next = new_node
+        new_node.prev = self.curr
+        self.curr = new_node
+
+    def back(self, steps: int) -> str:
+        curr = self.curr
+        while curr and steps > 0:
+            if curr.val == self.homepage:
+                return curr.val
+            curr = curr.prev
+            steps -= 1
+        self.curr = curr
+        return curr.val
+
+    def forward(self, steps: int) -> str:
+        curr = self.curr
+        while curr.next and steps > 0:
+            curr = curr.next
+            steps -= 1
+        self.curr = curr
+        return curr.val
+        
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
