84fd43a398ee3dbd6ad1498e49ff6b04ecde1646
Toni Dezman <toni.dezman@gmail.com>
2025-03-07 11:21:20 +0100

    LC-number-of-students-unable-to-eat-lunch [Runtime: 0 ms; Memory: 17.8 MB]

--- array/number-of-students-unable-to-eat-lunch.py
+++ array/number-of-students-unable-to-eat-lunch.py
@@ -1,15 +1,11 @@
-from collections import deque
-
+from collections import Counter
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        students = deque(students)
-        sandwiches = deque(sandwiches)
-        for _ in range(len(sandwiches)*5):
-            if len(students) == 0:
-                return 0
-            student = students.popleft()
-            if student == sandwiches[0]:
-                sandwiches.popleft()
-                continue
-            students.append(student)
-        return len(students)
+        counter = Counter(students)
+        for sandwich in sandwiches:
+            if counter[sandwich] > 0:
+                counter[sandwich] -= 1
+            else:
+                return sum(counter.values())
+        return sum(counter.values())
+        
