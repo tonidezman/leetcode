33fd323f3449fdf9ff25f03604b159db5f2333ec
Toni Dezman <toni.dezman@gmail.com>
2025-03-07 11:04:26 +0100

    LC-number-of-students-unable-to-eat-lunch [Runtime: 3 ms; Memory: 17.9 MB]

---
+++ queue/number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        students = deque(students)
+        sandwiches = deque(sandwiches)
+        for _ in range(1_000):
+            if len(students) == 0:
+                return 0
+            student = students.popleft()
+            if student == sandwiches[0]:
+                sandwiches.popleft()
+                continue
+            students.append(student)
+        return len(students)
