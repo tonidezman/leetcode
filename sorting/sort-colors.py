06e3149b9616095b520d87e9203e12e3dc849c56
Toni Dezman <toni.dezman@gmail.com>
2025-03-09 12:04:11 +0100

    LC-sort-colors [Runtime: 0 ms; Memory: 17.6 MB]

--- sorting/sort-colors.py
+++ sorting/sort-colors.py
@@ -1,13 +1,20 @@
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        counter = [0, 0, 0]
-        for num in nums:
-            counter[num] += 1
-        
+        left_boundary = 0
+        right_boundary = len(nums)-1
         i = 0
-        while i < len(nums):
-            for j in range(len(counter)):
-                for _ in range(counter[j]):
-                    nums[i] = j
-                    i += 1
-        
+        while i <= right_boundary:
+            if nums[i] == 0:
+                swap(nums, i, left_boundary)
+                left_boundary += 1
+                i += 1
+            elif nums[i] == 1:
+                i += 1
+            else:
+                swap(nums, i, right_boundary)
+                right_boundary -= 1
+            
+               
