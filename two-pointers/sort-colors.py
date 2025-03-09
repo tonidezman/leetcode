49e5a36defffbe79d7cf6deb25abcbc86c500378
Toni Dezman <toni.dezman@gmail.com>
2025-03-09 11:54:43 +0100

    LC-sort-colors [Runtime: 0 ms; Memory: 17.7 MB]

--- two-pointers/sort-colors.py
+++ two-pointers/sort-colors.py
@@ -1,18 +1,13 @@
-def swap(arr, i, j):
-    arr[i], arr[j] = arr[j], arr[i]
-    
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        l_boundary = 0
-        r_boundary = len(nums)-1
+        counter = [0, 0, 0]
+        for num in nums:
+            counter[num] += 1
+        
         i = 0
-        while i <= r_boundary: 
-            if nums[i] == 0:
-                swap(nums, i, l_boundary)
-                l_boundary += 1
-                i += 1
-            elif nums[i] == 1:
-                i += 1
-            else:
-                swap(nums, i, r_boundary)
-                r_boundary -= 1
+        while i < len(nums):
+            for j in range(len(counter)):
+                for _ in range(counter[j]):
+                    nums[i] = j
+                    i += 1
+        
