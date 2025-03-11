4fe8c6889c4d49d74d809aca4dad784ae94293e0
Toni Dezman <toni.dezman@gmail.com>
2025-03-11 17:46:04 +0100

    LC-delete-node-in-a-bst [Runtime: 0 ms; Memory: 21.5 MB]

--- tree/delete-node-in-a-bst.py
+++ tree/delete-node-in-a-bst.py
@@ -5,28 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findMin(self, root):
-        curr = root
-        while curr.left:
-            curr = curr.left
-        return curr
-
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if root is None:
             return
-        if root.val < key:
+        if key > root.val:
             root.right = self.deleteNode(root.right, key)
-        elif root.val > key:
+        elif key < root.val:
             root.left = self.deleteNode(root.left, key)
         else:
             if root.left is None:
                 return root.right
             elif root.right is None:
                 return root.left
-
+            
             curr = root.right
             while curr.left:
                 curr = curr.left
             root.val = curr.val
-            root.right = self.deleteNode(root.right, curr.val)
+            root.right = self.deleteNode(root.right, root.val)
         return root
