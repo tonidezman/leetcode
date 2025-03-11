de37533e4219440e20acd3cbd17b687c9e3a159f
Toni Dezman <toni.dezman@gmail.com>
2025-03-11 08:58:51 +0100

    LC-first-bad-version [Runtime: 24 ms; Memory: 17.7 MB]

--- binary-search/first-bad-version.py
+++ binary-search/first-bad-version.py
@@ -7,8 +7,7 @@ class Solution:
         res = -1
         while l <= r:
             mid = l + (r-l) // 2
-            is_bad = isBadVersion(mid)
-            if is_bad:
+            if isBadVersion(mid):
                 res = mid
                 r = mid - 1
             else:
