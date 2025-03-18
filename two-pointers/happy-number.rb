065a01da9d450a4833f4b88ec2cafb9e76c06206
Toni Dezman <toni.dezman@gmail.com>
2025-03-19 00:24:00 +0100

    LC-happy-number [Runtime: 0 ms; Memory: 211.8 MB]

--- two-pointers/happy-number.rb
+++ two-pointers/happy-number.rb
@@ -3,7 +3,7 @@
 def is_happy(n)
     1.upto(10) do
         return true if n == 1
-        n = n.to_s.split('').map(&:to_i).map { |n| n ** 2}.sum
+        n = n.to_s.split('').map { |n| n.to_i ** 2}.sum
     end
     false
 end
