3175153b0a2dc39fc605ee163021ef8b8b203a65
Toni Dezman <toni.dezman@gmail.com>
2025-03-18 17:17:45 +0100

    LC-score-of-a-string [Runtime: 0 ms; Memory: 211.4 MB]

--- string/score-of-a-string.rb
+++ string/score-of-a-string.rb
@@ -1,9 +1,5 @@
 # @param {String} s
 # @return {Integer}
 def score_of_string(s)
-    res = 0
-    0.upto(s.size-2) do |i|
-        res += (s[i].ord - s[i+1].ord).abs
-    end
-    res
+    0.upto(s.size-2).reduce(0) { |acc, i| acc + (s[i].ord - s[i+1].ord).abs }
 end
