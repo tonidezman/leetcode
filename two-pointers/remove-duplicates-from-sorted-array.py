bd49b74870cc824aa6e9c579053b66a77af2f535
Toni Dezman <toni.dezman@gmail.com>
2025-04-11 09:50:43 +0200

    LC-remove-duplicates-from-sorted-array [Runtime: 0 ms; Memory: 19 MB]

--- two-pointers/remove-duplicates-from-sorted-array.py
+++ two-pointers/remove-duplicates-from-sorted-array.py
@@ -1,17 +1,17 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        i = j = 0
-        while j < len(nums):
+        i = 0 
+        for j in range(len(nums)):
             if nums[j] != nums[i]:
+                nums[i+1] = nums[j]
                 i += 1
-                nums[i] = nums[j]
-            j += 1
         return i+1
 
+    
 
-
+       
 """
-[1,2,2]
-   ^
-       ^
+[1,2,3,4,3,3,4]
+               ^
+         ^
 """
