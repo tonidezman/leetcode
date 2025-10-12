b5d59ac9370a419f7b7ba2ac56501472b8f61b68
Toni Dezman <toni.dezman@gmail.com>
2025-10-12 17:36:55 +0200

    LC-reverse-linked-list [Runtime: 3 ms; Memory: 18.1 MB]

--- recursion/reverse-linked-list.py
+++ recursion/reverse-linked-list.py
@@ -9,11 +9,12 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        prev = None
-        curr = head
-        while curr:
-            tmp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = tmp
-        return prev
+        return self.rec(head)
+
+    def rec(self, head):
+        if not head or not head.next:
+            return head
+        new_head = self.rec(head.next)
+        head.next.next = head
+        head.next = None
+        return new_head
